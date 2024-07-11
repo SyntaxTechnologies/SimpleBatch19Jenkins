aed578e0881828c96d450d585ec324afae05d8ba
Asghar Nazir <asghar@syntaxtechs.com>
2024-07-10 21:20:59 -0400

    new code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Batch 19 is great");
+            Thread.sleep(2000);
+        }
+
     }
 }
+// 8:51
